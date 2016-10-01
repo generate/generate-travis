cf52140aeaeba24915f6f19d1086f2f8739c0e40
jonschlinkert <jon.schlinkert@sellside.com>
2016-10-01 06:27:34 -0400

    simplify templates

--- templates/defaults.js
+++ templates/defaults.js
@@ -3,31 +3,13 @@
 exports.json = {
   sudo: false,
   language: 'node_js',
-  node_js: [ '6', '5', '4', '0.12', '0.10' ],
-  matrix: {
-    fast_finish: true,
-    allow_failures: [
-      {node_js: '4'},
-      {node_js: '0.10'},
-      {node_js: '0.12'}
-    ]
-  }
+  node_js: ['node', '6' ]
 };
 
 exports.yaml = [
   'sudo: false',
   'language: node_js',
   'node_js:',
-  '  - "6"',
-  '  - "5"',
-  '  - "4"',
-  '  - "0.12"',
-  '  - "0.10"',
-  'matrix:',
-  '  fast_finish: true',
-  '  allow_failures:',
-  '    - node_js: "4"',
-  '    - node_js: "0.10"',
-  '    - node_js: "0.12"',
-  ''
+  '  - \'node\'',
+  '  - \'6\''
 ].join('\n');
